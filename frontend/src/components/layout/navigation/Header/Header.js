0f75faebf5d95b8189975e56a8df57050d8cdb9b
Dinija Seferović <dseferovic2@etf.unsa.ba>
2022-06-27 17:41:43 +0200

    Add Header component

---
+++ frontend/src/components/layout/navigation/Header/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import classes from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <div className={classes.header}>
+      <div className={classes.header_socialmedia}> 
+        <ul>
+            <li>
+                <a href="https://facebook.com" target="_blank" rel="noreferrer">
+                    <img
+                        src={require('../../../../assets/images/icons/facebook-icon.png')}
+                        alt="facebook"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="https://instagram.com" target="_blank" rel="noreferrer">
+                    <img
+                        src={require('../../../../assets/images/icons/instagram-icon.png')}
+                        alt="instagram"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="https://twitter.com" target="_blank" rel="noreferrer">
+                    <img
+                        src={require('../../../../assets/images/icons/twitter-icon.png')}
+                        alt="twitter"
+                    />
+                </a>
+            </li>
+        </ul>
+      </div>
+      <div className={classes.header_righttext}>
+        <h5>Hi, John Doe</h5>
+      </div>
+    </div>
+  )
+};
+
+export default Header;
