86813abe51ee291095a1e7cc09679846aa52bf8d
Dinija Seferović <dseferovic2@etf.unsa.ba>
2022-06-28 17:17:20 +0200

    Add main navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/layout/navigation/Header/Header';
+import MainNavBar from './components/layout/navigation/MainNavBar/MainNavBar';
 
 function App() {
   return (
     <> 
       <Header />
+      <MainNavBar />
     </>
   );
 }
--- frontend/src/components/layout/navigation/MainNavBar/MainNavBar.js
+++ frontend/src/components/layout/navigation/MainNavBar/MainNavBar.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import classes from './MainNavBar.module.scss';
+import Search from './Search/Search';
 
 const MainNavBar = () => {
   return (
-    <div>
-      
+    <div className={classes.mainnavbar}>
+      <div className={classes.logo}>
+        <img
+              src={require('../../../../assets/images/logos/logo-small.png')}
+              alt="logo"
+          />
+      </div>
+      <div className={classes.searchbar}>
+        <Search />
+      </div>
+      <div className={classes.navbar}>
+        <nav>
+        <ul>
+            <li>
+              <a href="/">HOME</a>
+            </li>
+            <li>
+              <a href="/">SHOP</a>
+            </li>
+            <li>
+              <a href="/">MY ACCOUNT</a>
+            </li>
+        </ul>
+        </nav>
+      </div>
     </div>
   )
 }
