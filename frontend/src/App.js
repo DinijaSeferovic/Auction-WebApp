75e5893f99e483be2ed72e4acd89cfd840fe7cb2
Dinija Seferović <dseferovic2@etf.unsa.ba>
2022-07-01 14:29:03 +0200

    Prepare router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import Header from './components/layout/navigation/Header/Header';
 import MainNavBar from './components/layout/navigation/MainNavBar/MainNavBar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/layout/navigation/Footer/Footer';
 
 function App() {
   return (
     <> 
-      <Header />
-      <MainNavBar />
+      <BrowserRouter>
+        <Header />
+        <MainNavBar />
+        <Footer />
+        <Routes>
+          {/*<Route index element={<Home />} />
+          <Route path="aboutus" element={<AboutUs />} />
+          <Route path="terms" element={<Terms />} />
+          <Route path="privacypolicy" element={<Privacy />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="account" element={<Account />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
