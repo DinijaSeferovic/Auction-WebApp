89bc6148a46987c5863d1a27c2381b1c54a3c6d6
Dinija Seferović <dseferovic2@etf.unsa.ba>
2022-07-01 14:29:47 +0200

    Change routes

--- frontend/src/components/layout/navigation/MainNavBar/MainNavBar.js
+++ frontend/src/components/layout/navigation/MainNavBar/MainNavBar.js
@@ -1,34 +1,36 @@
 import React from 'react'
 import classes from './MainNavBar.module.scss';
 import Search from './Search/Search';
+import {NavLink } from "react-router-dom";
 
 const MainNavBar = () => {
   return (
     <div className={classes.mainnavbar}>
-      <div className={classes.logo}>
-        <img
-              src={require('../../../../assets/images/logos/logo-small.png')}
-              alt="logo"
-          />
-      </div>
-      <div className={classes.searchbar}>
-        <Search />
-      </div>
-      <div className={classes.navbar}>
-        <nav>
-        <ul>
-            <li>
-              <a href="/">HOME</a>
-            </li>
-            <li>
-              <a href="/">SHOP</a>
-            </li>
-            <li>
-              <a href="/">MY ACCOUNT</a>
-            </li>
-        </ul>
-        </nav>
-      </div>
+		<div className={classes.mainnavbar_logo}>
+			<img
+				src={require('../../../../assets/images/logos/logo-small.png')}
+				alt="logo"
+			/>
+		</div>
+		<div className={classes.mainnavbar_searchbar}>
+			<Search />
+		</div>
+		<div className={classes.mainnavbar_navbar}>
+			<nav>
+				<ul>
+					<li>
+						<NavLink to="/"  className={classes.navlink}>HOME</NavLink>
+					</li>
+					{/* 
+					<li>
+						<NavLink to="/shop">SHOP</NavLink>
+					</li>
+					<li>
+						<NavLink to="/account">MY ACCOUNT</NavLink>
+					</li>*/}
+				</ul>
+			</nav>
+		</div>
     </div>
   )
 }
