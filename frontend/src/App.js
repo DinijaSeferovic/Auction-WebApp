54d3cb3dd72c7476cf6f87400394ad379c63e6c7
Dinija Seferović <dseferovic2@etf.unsa.ba>
2022-07-03 14:56:47 +0200

    Add new routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/layout/navigation/Header/Header';
 import MainNavBar from './components/layout/navigation/MainNavBar/MainNavBar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.scss';
 import Footer from './components/layout/Footer/Footer';
 import TermsAndConditions from './views/Static/TermsAndConditions/TermsAndConditions';
 import AboutUs from './views/Static/AboutUs/AboutUs';
 import PrivacyAndPolicy from './views/Static/PrivacyAndPolicy/PrivacyAndPolicy';
-import './App.scss';
+import NotFound from './views/Static/NotFound/NotFound';
+import Home from './views/Home/Home';
 
 function App() {
   return (
@@ -15,13 +17,15 @@ function App() {
 				<Header />
 				<MainNavBar />
 				<Routes>
+					<Route path="/" element={<Home />} />
 					<Route path="aboutus" element={<AboutUs />} />
 					<Route path="termsandconditions" element={<TermsAndConditions />} />
 					<Route path="privacypolicy" element={<PrivacyAndPolicy />} />
+					<Route path="*" element={<NotFound />} /> 
 					{/* 
 					<Route path="shop" element={<Shop />} />
 					<Route path="account" element={<Account />} />
-					<Route path="*" element={<NoPage />} /> */}
+					*/}
 				</Routes>
 			</div>
 			<Footer />
