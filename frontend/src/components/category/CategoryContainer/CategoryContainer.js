149a2bfeb5ac5ebc67789b5997595561ae7b05c3
Dinija Seferović <dseferovic2@etf.unsa.ba>
2022-07-15 17:25:22 +0200

    Add necessary components

--- frontend/src/components/category/CategoryContainer/CategoryContainer.js
+++ frontend/src/components/category/CategoryContainer/CategoryContainer.js
@@ -7,9 +7,25 @@ const CategoryContainer = () => {
 	const [categories, setCategories] = useState([]);
 
 	useEffect(() => {
+<<<<<<< HEAD
 		categoryService
 			.getMainCategories()
 			.then((response) => setCategories(response));
+=======
+		fetch("/api/category/main-categories")
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(
+						`This is an HTTP error: The status is ${response.status}`
+					);
+				}
+				return response.json();
+			})
+			.then((data) => setCategories(data))
+			.catch((err) => {
+				console.log(err.message);
+			});
+>>>>>>> 3b3ce53... Add necessary components
 	}, []);
 
 	return (
@@ -19,7 +35,11 @@ const CategoryContainer = () => {
 				{categories.map((category) => (
 					<li key={category.id}>
 						<div className={classes.category_container_item}>
+<<<<<<< HEAD
 							<Category name={category.name} />
+=======
+							<Category id={category.id} name={category.name} />
+>>>>>>> 3b3ce53... Add necessary components
 						</div>
 					</li>
 				))}
--- frontend/src/components/container/CategoryHighlightContainer/CategoryHighlightContainer.js
+++ frontend/src/components/container/CategoryHighlightContainer/CategoryHighlightContainer.js
@@ -1,11 +1,11 @@
-import CategoryContainer from "../category/CategoryContainer/CategoryContainer";
-import ProductHighlight from "../product/ProductHighlight/ProductHighlight";
+import CategoryContainer from "../../category/CategoryContainer/CategoryContainer";
+import ProductHighlight from "../../product/ProductHighlight/ProductHighlight";
 import classes from "./CategoryHighlightContainer.module.scss";
 
 const CategoryHighlightContainer = () => {
 	return (
 		<div className={classes.container}>
-			<CategoryContainer name="Categories" />
+			<CategoryContainer />
 			<ProductHighlight />
 		</div>
 	);
