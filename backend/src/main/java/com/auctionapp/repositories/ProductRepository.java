7ca4e80a2d7c199c62e3f8a7f4e5f83bfd5d9602
Dinija Seferović <dseferovic2@etf.unsa.ba>
2022-07-19 00:44:04 +0200

    Apply backend changes

--- backend/src/main/java/com/auctionapp/repositories/ProductRepository.java
+++ backend/src/main/java/com/auctionapp/repositories/ProductRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
@@ -15,11 +15,6 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     Page< Product > findByEndDateAfter(LocalDateTime date, Pageable pageable);
 
     Product findTopByEndDateAfterOrderByEndDateAsc(LocalDateTime date);
-=======
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    Page<Product> findAll(Pageable pageable);
-    Product findTopByOrderByIdDesc();
 
->>>>>>> f5b062e... Add product classes
+    Product findById(UUID id);
 }
--- backend/src/main/java/com/auctionapp/services/ProductService.java
+++ backend/src/main/java/com/auctionapp/services/ProductService.java
@@ -1,49 +1,40 @@
 package com.auctionapp.services;
 
 import com.auctionapp.domains.Product;
-<<<<<<< HEAD
 import com.auctionapp.dto.ProductDTO;
 import com.auctionapp.repositories.ProductRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-=======
-import com.auctionapp.repositories.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> f5b062e... Add product classes
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
-=======
-import java.util.List;
->>>>>>> f5b062e... Add product classes
 
 @Service
 public class ProductService  {
 
     private final ProductRepository productRepository;
 
-<<<<<<< HEAD
     private final ModelMapper modelMapper = new ModelMapper();
 
-=======
-    @Autowired
->>>>>>> f5b062e... Add product classes
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-<<<<<<< HEAD
     public ProductDTO getHighlightProduct() {
         return convertEntityToDto(productRepository.findTopByEndDateAfterOrderByEndDateAsc(LocalDateTime.now()));
     }
 
+    public ProductDTO getProduct(UUID id) {
+        return convertEntityToDto(productRepository.findById(id));
+    }
+    
     public List< ProductDTO > getProductsLastChance(int page, int limit){
         Pageable paging = PageRequest.of(page, limit, Sort.by(Sort.Order.asc("endDate")));
         Page< Product > pagedResult = productRepository.findByEndDateAfter(LocalDateTime.now(), paging);
@@ -78,28 +69,3 @@ public class ProductService  {
         return productDTO;
     }
 }
-
-=======
-    public List<Product> getProducts() {
-        return productRepository.findAll();
-    }
-
-    public Product getHighlightProduct() {
-        return productRepository.findTopByOrderByIdDesc();
-    }
-
-    public void addNewProduct(Product product) {
-    }
-    public List<Product> getProductsLastChance(int page, int limit) {
-        Pageable paging = PageRequest.of(page, limit, Sort.by(Sort.Order.asc("endDate")));
-        Page<Product> pagedResult = productRepository.findAll(paging);
-        return pagedResult.toList();
-    }
-
-    public List<Product> getProductsNewArrivals(int page, int limit) {
-        Pageable paging = PageRequest.of(page, limit, Sort.by(Sort.Order.desc("startDate")));
-        Page<Product> pagedResult = productRepository.findAll(paging);
-        return pagedResult.toList();
-    }
-}
->>>>>>> f5b062e... Add product classes
